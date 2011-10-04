d04b36c898a9e9666656f8dffc9a9daabb5ed879
Alexandre Passos <alexandre.tp@gmail.com>
2011-10-04 17:59:26 -0400

    more progress in parsing classes

--- parseClass.js
+++ parseClass.js
@@ -56,19 +56,78 @@ function readConstant(f) {
 }
 
 function validateConstants(clist) {
-    
+    for (var i = 1; i < clists.length; ++i) {
+	var tag = clist[i][0]
+	if (tag == "class") {
+	    assert(clist[clist[i][1]][0] == "utf")
+	    clist[i][1] = clist[clist[i][1]][1] // directly associating a class with its name
+	} else if ((tag == "fref") || (tag == "mref") || (tag == "imref")) {
+	    assert(clist[clist[i][1]][0] == "class")
+	    clist[i][1] = clist[clist[i][1]][1]
+	    assert(clist[clust[i][2]][0] == "nt")
+	} else if (tag == "string") {
+	    assert(clist[clist[i][1]][0] =="utf")
+	    clist[i][1] = clist[clist[i][1]][1]
+	} else if (tag == "nt") {
+	    assert(clist[clist[i][1]][0] == "utf")
+	    clist[i][1] = clist[clist[i][1]][1]
+	    assert(clist[clist[i][2]][0] == "utf")
+	    clist[i][2] = clist[clist[i][2]][1]
+	}
+    }
+    for (var i = 1; i < clist.length; ++i) {
+	var tag = clist[i][0]
+	if (tag == "fref")
+	    validateTypeName(clist[i][2])
+	else if ((tag == "mref") || (tag == "imref"))
+	    validateMethodName(clist[i][2])
+    }
 }
+
+var typere = /(\[)*(([BCDFIJSZ])|(L[A-Za-z0-9\/]+;))/  ///sdfsdf
+var validateTypeName = typere.test
+
+var methodre = /\(((\[)*(([BCDFIJSZ])|(L[A-Za-z0-9\/]+;)))*\)(((\[)*(([BCDFIJSZ])|(L[A-Za-z0-9\/]+;)))|V)/  ///sdfsdf
+var validateMethodName = methodre.test
+
+var ACC_PUBLIC = 0x0001 // Declared public; may be accessed from outside its package.
+var ACC_PRIVATE = 0x0002 // Declared private; usable only within the defining class.
+var ACC_PROTECTED = 0x0004 // Declared protected; may be accessed within subclasses.
+var ACC_STATIC = 0x0008 // Declared static.
+var ACC_FINAL = 0x0010 // Declared final; no further assignment after initialization.
+var ACC_VOLATILE = 0x0040 // Declared volatile; cannot be cached.
+var ACC_TRANSIENT = 0x0080 // Declared transient; not written or read by a persistent object manager.
+
 
 function parseClass(fname) {
     var f = new java.io.RandomAccessFile(fname, "r")
     assert(f.readInt() == 0xcafebabe)
     f.readInt() // ignore the version of the format
-    var cpcount = readShort(f)
+    var cpcount = f.readShort()
     var constants = [0] // to make indexing start at 1
     for(var i = 1; i < cpcount; ++i) {
 	constants.push(readConstant(f))
+	var t = constants[constants.length-1]
+	if ((t == "long") || (t == "double")) {
+	    constants.push(0)
+	    i += 1 // dealing with the fact that the constant pool count is wrong
+	}
     }
     validateConstants(constants)
-    
+    var aflags = f.readShort()
+    var ths = constants[f.readShort()][1]
+    var supr = constants[f.readShort()][1]
+    var icount = f.readShort()
+    var interfaces = []
+    for (var i = 0; i < icount; ++i) {
+	var flags = f.readShort()
+	var name = constants[f.readShort()][1]
+	var descr = constants[f.readShort()][1]
+	var acount = f.readShort()
+	var attrs = []
+	for (var j = 0; j < acount; ++j) {
+	    
+	}
+    }
     
 }
